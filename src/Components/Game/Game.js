f758f27553dbda290fa03b92c6df236dfe6f8b0f
Venetrius <gergelygjuhasz@gmail.com>
2019-05-21 23:08:39 -0600

    add some styleing w/ react-bootstrap

--- src/Components/Game/Game.js
+++ src/Components/Game/Game.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import Board from './Board/Board.js';
 import GameLogic from './../../Logic/game-logic.js';
+import { Button, Navbar, Container, Row,Col } from 'react-bootstrap';
+import './Game.css';
+
 
 class Game extends Component{
 
   constructor(props){
     super(props);
     this.state = {
+      started : false,
       isPlayerTurn : true,
       dimension: 10,
       fields: this.initFields(10),
@@ -15,18 +19,34 @@ class Game extends Component{
   }
 
   render(){
-    return (
+    const game = 
       <div>
-        it is {(this.state.isPlayerTurn ? "the player's" : "the AI's")} turn 
-        <br/>
-        <button onClick={() => this.restartGame()}> RestartGame</button>
-        your board
-        <Board type="own" fields={this.state.fields}/>
-        enemy board
-        <Board changeTurn={(x,y)=>this.handleFieldClick(x,y)} enemyTurn = {() => this.handleEnemyCommand()} 
-          isPlayerTurn={this.state.isPlayerTurn} type="enemy" fields={this.state.enemy.fields}></Board>        
+        {this.header()}
+        <h2>it is {(this.state.isPlayerTurn ? "the player's" : "the AI's")} turn </h2>
+        <Container>
+          <Row>
+            <Col>
+              <h3>your board</h3>
+              <Board type="own" fields={this.state.fields}/>
+              
+            </Col>
+            <Col>
+              <h3>enemy board</h3>
+              <Board changeTurn={(x,y)=>this.handleFieldClick(x,y)} enemyTurn = {() => this.handleEnemyCommand()} 
+                isPlayerTurn={this.state.isPlayerTurn} type="enemy" fields={this.state.enemy.fields}>
+              </Board>        
+            </Col>
+          </Row>
+        </Container>
       </div>
-    );
+    return ( game    );
+  }
+
+  header(){
+    return (
+    <Navbar bg="light" expand="lg">
+     <span> <h1> Battle Ship </h1> </span> <Button variant="danger" onClick={() => this.restartGame()}> Start a new Game</Button>
+    </Navbar>);
   }
 
   restartGame(){
